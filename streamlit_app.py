983fcbb1073214c99da663d0284fb394e4a79799
Zain Al Abidin <113837968+zain2983@users.noreply.github.com>
2024-11-03 10:44:33 +0000

    updated the app

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("🎈 Zain Ki Test App")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
